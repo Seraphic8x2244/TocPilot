9ee2245d02d296dd517169f81beb7cb36f2d5a26
Seraphic8x2244 <52291933+Seraphic8x2244@users.noreply.github.com>
2026-09-20 17:30:59 +0100

    Expose smart HTTP repository ref discovery

--- src/git_refs.h
+++ src/git_refs.h
@@ -2,8 +2,30 @@
 
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace tp {
+
+struct GitRemoteBranch {
+    std::wstring name;
+    std::wstring sha;
+};
+
+struct GitRemoteRepositoryInfo {
+    std::wstring defaultBranch;
+    std::vector<GitRemoteBranch> branches;
+};
+
+bool ParseGitSmartHttpRepositoryAdvertisement(
+    std::string_view advertisement,
+    GitRemoteRepositoryInfo& info,
+    std::wstring& error);
+
+bool FetchPublicGitRepositoryInfo(
+    std::wstring_view host,
+    std::wstring_view repository,
+    GitRemoteRepositoryInfo& info,
+    std::wstring& error);
 
 bool ParseGitSmartHttpBranchAdvertisement(
     std::string_view advertisement,
