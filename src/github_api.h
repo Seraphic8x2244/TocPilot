077db284a0f6d2e80213ba5c0b0639782d699326
Seraphic8x2244 <52291933+Seraphic8x2244@users.noreply.github.com>
2026-09-20 17:33:10 +0100

    Remove obsolete GitHub REST branch API surface

--- src/github_api.h
+++ src/github_api.h
@@ -18,22 +18,6 @@ struct GitHubRepositoryInfo {
     std::vector<GitHubBranch> branches;
 };
 
-bool ParseGitHubRepositoryJson(
-    std::string_view json,
-    std::wstring& defaultBranch,
-    std::wstring& error);
-
-bool ParseGitHubBranchesJson(
-    std::string_view json,
-    std::vector<GitHubBranch>& branches,
-    std::wstring& error);
-
-bool FindGitHubBranchHead(
-    const std::vector<GitHubBranch>& branches,
-    std::wstring_view branch,
-    std::wstring& remoteSha,
-    std::wstring& error);
-
 bool FetchGitHubRepositoryInfo(
     std::wstring_view repository,
     GitHubRepositoryInfo& info,
@@ -43,12 +27,6 @@ bool ResolveGitHubBranchHead(
     std::wstring_view repository,
     std::wstring_view branch,
     std::wstring& remoteSha,
-    std::wstring& error);
-
-bool BuildGitHubArchiveApiPath(
-    std::wstring_view repository,
-    std::wstring_view ref,
-    std::wstring& apiPath,
     std::wstring& error);
 
 bool BuildGitHubCodeloadPath(
