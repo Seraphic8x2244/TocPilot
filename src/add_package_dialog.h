2edc5ca1b60346748db28092b639289faa249948
Seraphic8x2244 <52291933+Seraphic8x2244@users.noreply.github.com>
2026-09-23 18:37:14 +0100

    Expose Add Git DLL fallback dialog

--- src/add_package_dialog.h
+++ src/add_package_dialog.h
@@ -4,10 +4,20 @@
 
 #include <windows.h>
 
+#include <string>
+#include <string_view>
+
 namespace tp {
 
 bool ShowAddPackageDialog(
     HWND owner,
     PackageRecord& package);
+
+bool ShowDirectDllFallbackDialog(
+    HWND owner,
+    std::wstring_view repository,
+    PackageRecord& package,
+    bool& supportedDllFound,
+    std::wstring& error);
 
 } // namespace tp
